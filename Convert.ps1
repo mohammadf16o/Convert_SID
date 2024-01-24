f0400a105f6276536ec601f0bf981a85eba38860
mohammadf16o <44226060+mohammadf16o@users.noreply.github.com>
2024-01-24 22:49:09 +0300

    Add files via upload

---
+++ Convert.ps1
@@ -0,0 +1,10 @@
+﻿$User_SID=$args[0]
+
+# Give SID as input to .NET Framework Class
+$SID = New-Object System.Security.Principal.SecurityIdentifier("$User_SID")
+
+# Use Translate to find user from sid
+$objUser = $SID.Translate([System.Security.Principal.NTAccount])
+
+# Print the converted SID to username value
+$objUser.Value
